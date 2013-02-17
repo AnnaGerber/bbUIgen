ed06097927eebdc31a2fe08d152706aa17c70560
Anna Gerber <anna.m.gerber@gmail.com>
2013-02-17 15:28:23 +1000

    template for app script

--- bbUIgen/__appName__.js
+++ bbUIgen/__appName__.js
@@ -3,3 +3,17 @@
  * Copyright {{year}} {{author}}
  * {{authorURL}}
  */
+window.addEventListener('DOMContentLoaded', function () {
+   var webworksreadyFired = false;
+   document.addEventListener('webworksready', function () {
+       if(webworksreadyFired) return;
+       webworksreadyFired = true;
+       if (typeof bb !== 'undefined') {
+           bb.init();
+	   // TODO: Auto-generated stub - implement app behaviour here
+	   {{#appScreens}}
+	      bb.pushScreen('{{id}}.html','{{id}}');
+           {{/appScreens}}
+       }
+   },false);
+},false);
