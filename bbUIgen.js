e8663104df7e2579604f8ee360150bf7e88c1e64
Anna Gerber <anna.m.gerber@gmail.com>
2013-02-17 15:09:28 +1000

    Templates for app screen html files

--- bbUIgen.js
+++ bbUIgen.js
@@ -5,15 +5,26 @@ fgen.createGenerator("bbUIgen", function(err, generator) {
 	    console.log(err);
 	    return;
 	}
-
-	generator.context = require("./myApp.js");
-	generator.generateAll("./output/" + generator.context.appName, function(err) {
-		if (!err) {
-		    console.log("Successful generation.");
-		}
+        var globalContext = require("./myApp.js");
+        generator.context = globalContext;
+        generator.generateAll("./output/" + generator.context.appName,
+           function(k) {return k!="__appScreens__.html";},
+           function(err) {
+                if (!err) {
+                    console.log("Successful generation.");
+                }
                 else {
-		    console.log("Error during generation: ",err);
-		}
-	});
-
-    });
+                    console.log("Error during generation: ",err);
+                }
+          }
+       );
+       if (globalContext.appScreens && globalContext.appScreens.length > 0) {
+            for (var i = 0; i < globalContext.appScreens.length; i++){
+                var appScreen = globalContext.appScreens[i];
+                generator.context=appScreen;
+                // generate files for app screens
+                generator.generate("__appScreens__.html",
+                  "./output/"+globalContext.appName + "/" + appScreen.id + ".html");
+            }
+       }
+});
--- myApp.js
+++ myApp.js
@@ -5,7 +5,25 @@ exports.author="Anonymous";
 exports.authorURL="http://www.example.com";
 
 // optional
-exports.description="My BB10 app";
+exports.description="My Sample BB10 app";
 exports.features=[
     {"id": "blackberry.pim.calendar"},
 ]
+exports.appScreens=[
+ {
+     "id":"menu",
+     "list":[
+        {
+            "listtype":"image-list",
+	    "items":[{"id":"createEvent","title":"Create Event"}]
+        }
+     ]
+ },
+ {
+     "id": "createEvent",
+     "actionbar": [
+       {"id":"add", "name":"Add"},
+       {"id":"help","name":"Help"}
+     ]
+ }
+];
